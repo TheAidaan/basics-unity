d9c980304ce0472bc51c42ad3278d84259064f52
TheAidaan <56072883+TheAidaan@users.noreply.github.com>
2023-03-28 16:53:33 +0200

    Task-1: WASD movement
    Add a cube and set its position so that it is in the middle of the camera view.
    • Write a script that moves the cube with physics (i.e. using a rigidbody on the cube) up,
    down, left and right using the WASD keys.

--- Project Files/Assets/00 - Game Root/Scripts/CubeMovement.cs
+++ Project Files/Assets/00 - Game Root/Scripts/CubeMovement.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class CubeMovement : MonoBehaviour
 {
+    const float MOVE_SPEED = 0.3f;
+    Rigidbody _rb;
+    Vector3 _movementDir;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.velocity = _movementDir.normalized * MOVE_SPEED; // move in the direction the player wants at a set speed
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _movementDir = Vector2.zero; 
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            _movementDir.y += 1f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+
+            _movementDir.x -= 1f;
+
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            _movementDir.y -= 1f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            _movementDir.x += 1f;
+        }
     }
 }
